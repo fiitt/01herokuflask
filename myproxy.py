af6ecf6f842543755cc89c1d96e1c019b1f749a4
fiitt <vvnfiit@gmail.com>
2020-10-12 10:26:29 +0900

    lab1

--- myproxy.py
+++ myproxy.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello FIIT-17!'
-
+	if 'url' in request.args:
+		r = requests.get(request.args['url'])
+		return  r.content
+	else:
+		return '''
+		<form method = 'get'>
+		<input type="text" name="url">
+		<input type="submit" value="Отправить">
+		</form>
+		'''
 if __name__ == '__main__':
-    app.run()
+	app.run()
